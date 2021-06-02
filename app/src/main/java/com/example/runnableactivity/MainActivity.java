8c05fcf374141ea0fb22f2b92406cfffb33d5d1d
JINNY.US <kyungjin2215@gmail.com>
2021-06-02 14:55:55 +0900

    Multi-threading 1 solution

--- app/src/main/java/com/example/runnableactivity/MainActivity.java
+++ app/src/main/java/com/example/runnableactivity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+/*
 public class MainActivity extends AppCompatActivity {
     Thread wr, wr2;
     boolean running = true;
@@ -74,4 +75,75 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+}*/
+
+public class MainActivity extends AppCompatActivity {
+    Thread wr;
+    WorkerThread wt;
+    boolean running = true;
+    String TAG2 = "THREAD2";
+    String TAG = "THREAD";
+
+
+    class WorkerThread extends  Thread{
+        @Override
+        public void run() {
+            int i =0;
+            for(i = 0; i<20 && running; i++){
+                try{
+                    Thread.sleep(1000);
+
+                }catch (InterruptedException e){
+
+                }
+                Log.v(TAG,"Thread time = "+i);
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        running = true;
+        wt= new WorkerThread();
+        wr = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i =0;
+                for(i = 0; i < 20 && running; i++){
+                    try{
+                        Thread.sleep(1000);
+
+                    }catch (InterruptedException e){
+
+                    }
+                    Log.v(TAG2,"Runnable time= "+i);
+                }
+            }
+        });
+        wr.start();
+        wt.start();
+        Log.v(TAG2,"Now I am in onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        running = false;
+        Log.v(TAG2,"Now I am in onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG2,"Now I am in onPause");
+    }
+
+
 }
