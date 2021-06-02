169c438a090b949e28f655fb75a0a2b992605f5e
JINNY.US <kyungjin2215@gmail.com>
2021-06-02 15:21:30 +0900

    Runnable by inner class (홍진님 프로젝트 기반 수정)

--- app/src/main/java/com/example/runnableactivity/MainActivity.java
+++ app/src/main/java/com/example/runnableactivity/MainActivity.java
@@ -5,77 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-/*
-public class MainActivity extends AppCompatActivity {
-    Thread wr, wr2;
-    boolean running = true;
-    String TAG1, TAG2 = "THREAD";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        wr2 = new WorkerThread();
-        running = true;
-
-        wr2.start();
-        Log.v(TAG1,"Now I am in onStart");
-        wr = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int i = 0;
-                for(i=0;i<20 && running;i++){
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    Log.v(TAG2,"Runnable time="+i);
-                }
-
-            }
-        });
-        wr.start();
-        Log.v(TAG2,"Now I am in onStart");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        running = false;
-        Log.v(TAG1,"Now I am in onStop");
-        Log.v(TAG2,"Now I am in onStop");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        running = false;
-        Log.v(TAG1,"Now I am in onPause");
-        Log.v(TAG2,"Now I am in onPause");
-    }
-
-
-    class WorkerThread extends Thread{
-        @Override
-        public void run() {
-            int i = 0 ;
-            for(i = 0 ; i < 20 && running ; i++){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Log.v(TAG1,"Thread times= " + i);
-            }
-        }
-    }
-}*/
 
 public class MainActivity extends AppCompatActivity {
     Thread wr;
@@ -84,19 +13,18 @@ public class MainActivity extends AppCompatActivity {
     String TAG2 = "THREAD2";
     String TAG = "THREAD";
 
-
     class WorkerThread extends  Thread{
         @Override
         public void run() {
             int i =0;
-            for(i = 0; i<20 && running; i++){
+            for(i = 0; i < 20 && running; i++){
                 try{
                     Thread.sleep(1000);
 
                 }catch (InterruptedException e){
 
                 }
-                Log.v(TAG,"Thread time = "+i);
+                Log.v(TAG,"Thread time= "+i);
             }
         }
     }
@@ -110,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        running = true;
+
         wt= new WorkerThread();
-        wr = new Thread(new Runnable() {
+        /*wr = new Thread(new Runnable() {
             @Override
             public void run() {
                 int i =0;
@@ -126,9 +54,11 @@ public class MainActivity extends AppCompatActivity {
                     Log.v(TAG2,"Runnable time= "+i);
                 }
             }
-        });
-        wr.start();
+        });*/
+        wr = new WorkerRunnable();
+        wr.run();
         wt.start();
+        running = true;
         Log.v(TAG2,"Now I am in onStart");
     }
 
@@ -146,4 +76,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private class WorkerRunnable extends Thread {
+        public void run() {
+            int i = 0;
+            for (i = 0; i < 20 && running; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.v(TAG, "Runnable times= " + i);
+            }
+        }
+    }
 }
